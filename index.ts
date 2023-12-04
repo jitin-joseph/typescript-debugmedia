ad38d86aafbcbc89b90d06e226b2c8c996f7e3c3
Jitin Joseph <jitin.joseph@toptechinfo.com>
2023-12-04 15:28:37 +0530

    interface

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ let age = 30;
 age = "Jitin";*/
 
 //Defining types (explicit types)
-let userName:string = "Jitin Joseph";
+/*let userName:string = "Jitin Joseph";
 let age:number = 30;
 let isSubscribed:boolean = true;
 let skills:string[] = ["PHP","HTML","CSS","JS"];//array of strings
@@ -18,4 +18,22 @@ let userDetail:{name:string; age:number; salary:number} = {
     age:30,
     salary:3000
 };
-userDetail.age=35;
+userDetail.age=35;*/
+
+//interface
+interface Details{
+    name:string;
+    age:number;
+    salary:number;
+}
+let userDetail:Details={
+    name:"Jitin Joseph",
+    age:30,
+    salary:3000
+};
+
+let adminDetail:Details={
+    name:"Admin",
+    age:34,
+    salary:3400
+};
