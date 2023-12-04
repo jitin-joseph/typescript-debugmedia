e567e83ed78e2c985369796096bdbace5328e87a
Jitin Joseph <jitin.joseph@toptechinfo.com>
2023-12-04 14:29:50 +0530

    infer and delcare types

---
+++ index.js
@@ -0,0 +1,20 @@
+//Infer types (implicit types)
+/*let userName = "Jitin Joseph";
+userName = 0;
+let age = 30;
+age = "Jitin";*/
+//Defining types (explicit types)
+var userName = "Jitin Joseph";
+var age = 30;
+var isSubscribed = true;
+var skills = ["PHP", "HTML", "CSS", "JS"]; //array of strings
+//let skills1:number[] = ["PHP","HTML","CSS","JS"];//error
+var count = [1, 2, 3, 4]; //array of numbers
+var emptyArray = [];
+//object
+var userDetail = {
+    name: "Jitin Joseph",
+    age: 30,
+    salary: 3000
+};
+userDetail.age = 35;
---
+++ index.ts
@@ -0,0 +1,21 @@
+//Infer types (implicit types)
+/*let userName = "Jitin Joseph";
+userName = 0;
+let age = 30;
+age = "Jitin";*/
+
+//Defining types (explicit types)
+let userName:string = "Jitin Joseph";
+let age:number = 30;
+let isSubscribed:boolean = true;
+let skills:string[] = ["PHP","HTML","CSS","JS"];//array of strings
+//let skills1:number[] = ["PHP","HTML","CSS","JS"];//error
+let count:number[]=[1,2,3,4];//array of numbers
+let emptyArray:[] = [];
+//object
+let userDetail:{name:string; age:number; salary:number} = {
+    name:"Jitin Joseph",
+    age:30,
+    salary:3000
+};
+userDetail.age=35;
